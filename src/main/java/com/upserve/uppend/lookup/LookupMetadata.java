1597384cde358869c0a91beca3d5507145e4a255
David Stuebe <stu3b3@gmail.com>
2018-05-29 23:01:59 -0400

    wip

--- src/main/java/com/upserve/uppend/lookup/LookupMetadata.java
+++ src/main/java/com/upserve/uppend/lookup/LookupMetadata.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class LookupMetadata {
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    private static final int MAX_BISECT_KEY_CACHE_DEPTH = 9;
+    private static final int MAX_BISECT_KEY_CACHE_DEPTH = 11;
 
     private final int metadataGeneration;
 
